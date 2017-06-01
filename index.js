164656bd35d814e768a3d86007258f483533f9ef
Gabriel <pabombs@gmail.com>
2017-05-31 22:30:42 -0400

    exposes compare function

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ const diip = require('diip');
 
 const conflict = 'conflict';
 
+const compare = (a, b) => {
+    let diff = diip(a, b);
+    if (diff !== null) {
+        if (diff[0].length === 0) {
+            return conflict;
+        }
+        for (let i = 0; i < diff.length; ++i) {
+            let address = diff[i];
+            let _a = a;
+            let _b = b;
+            for (let j = 0; j + 1 < address.length; ++j) {
+                _a = _a[address[j]];
+                _b = _b[address[j]];
+            }
+            let lastKey = address[address.length - 1];
+            if (_a[lastKey] == null) {
+                _a[lastKey] = _b[lastKey];
+                continue;
+            }
+            if (_b[lastKey] == null) {
+                continue;
+            }
+            if (_a[lastKey] != _b[lastKey]) {
+                _a[lastKey] = conflict;
+            }
+        };
+    }
+    return a;
+};
+
 const ence = (obj) => {
     try {
         obj = JSON.parse(JSON.stringify(obj));
     } catch (e) {
         throw new Error('Invalid input object format');
     }
-
-    const compare = (a, b) => {
-        let diff = diip(a, b);
-        if (diff !== null) {
-            if (diff[0].length === 0) {
-                return conflict;
-            }
-            for (let i = 0; i < diff.length; ++i) {
-                let address = diff[i];
-                let _a = a;
-                let _b = b;
-                for (let j = 0; j + 1 < address.length; ++j) {
-                    _a = _a[address[j]];
-                    _b = _b[address[j]];
-                }
-                let lastKey = address[address.length - 1];
-                if (_a[lastKey] == null) {
-                    _a[lastKey] = _b[lastKey];
-                    continue;
-                }
-                if (_b[lastKey] == null) {
-                    continue;
-                }
-                if (_a[lastKey] != _b[lastKey]) {
-                    _a[lastKey] = conflict;
-                }
-            };
-        }
-        return a;
-    };
 
     const scan = (obj) => {
         if (Array.isArray(obj)) {
@@ -67,5 +67,7 @@ const ence = (obj) => {
 
     return scan(obj);
 };
+
+ence.compare = compare;
 
 module.exports = ence;
