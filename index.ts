a206e6647504be8f792864a51970c6c1a132d697
Gabriel <gabrielj.harel@gmail.com>
2018-06-28 22:47:37 -0400

    added empty detection

--- index.ts
+++ index.ts
@@ -3,6 +3,16 @@ const arrayKey = "[n]";
 const typePrefix = " :: ";
 const typeSeparator = " | ";
 
+enum types {
+    E = "empty",
+    N = "null",
+    B = "boolean",
+    S = "string",
+    M = "number",
+    A = "array",
+    O = "object",
+}
+
 type Map<T> = {
     [key: string]: T;
 };
@@ -13,61 +23,139 @@ const joinKeys = (o: Map<boolean>) => {
         .join(typeSeparator);
 };
 
-class TypeInfo {
-    private store: Map<Map<boolean>> = {};
-
-    public add(address: string, type: string) {
-        let value = this.store[address];
-        if (value === undefined) {
-            value = this.store[address] = {};
-        }
-        value[type] = true;
-    }
-
-    public toString() {
-        return Object.keys(this.store)
-            .sort()
-            .map((address) => {
-                return address + typePrefix + joinKeys(this.store[address]);
-            })
-            .join("\n");
-    }
-}
-
-const addToInfo = (value: any, address: string, info: TypeInfo): void => {
-    // typeof null === "object"
+const isBasic = (value: any): string => {
     if (value === null) {
-        info.add(address, "null");
-        return;
+        return types.N;
     }
-
-    // typeof [] === "object"
-    if (Array.isArray(value)) {
-        info.add(address, "array");
-        const newAddr = address + arrayKey;
-        value.forEach((child) => {
-            addToInfo(child, newAddr, info);
-        });
-        return;
-    }
-
     const type = typeof value;
-    info.add(address, type);
+    if (type === "boolean") {
+        return types.B;
+    } else if (type === "number") {
+        return types.M;
+    } else if (type === "string") {
+        return types.S;
+    }
+    return "";
+};
 
-    if (type === "object") {
-        Object.keys(value).forEach((key) => {
-            addToInfo(value[key], address + addressSeparator + key, info);
+const joinInfo = (info: Map<Map<boolean>>) => {
+    return Object.keys(info)
+        .sort()
+        .map((address) => {
+            return address + typePrefix + joinKeys(info[address]);
+        })
+        .join("\n");
+};
+
+const createEntry = (info: Map<Map<boolean>>, address: string) => {
+    if (info[address] === undefined) {
+        info[address] = {};
+    }
+};
+
+const addToInfo = (info: Map<Map<boolean>>, address: string, type: string) => {
+    if (info[address] === undefined) {
+        info[address] = {};
+    }
+    info[address][type] = true;
+};
+
+const addToInfoExpect = (
+    info: Map<Map<boolean>>,
+    address: string,
+    type: string,
+) => {
+    if (info[address] === undefined) {
+        info[address] = {[types.E]: true};
+    }
+    info[address][type] = true;
+};
+
+const addArray = (
+    value: any,
+    address: string,
+    info: Map<Map<boolean>>,
+    firsts: Map<string[]>,
+) => {
+    if (value.length === 0) {
+        return;
+    }
+
+    const newAddr = address + arrayKey;
+    value.forEach((child: any) => {
+        const type = isBasic(child);
+        if (type !== "") {
+            addToInfo(info, newAddr, type);
+            return;
+        }
+        if (Array.isArray(child)) {
+            addToInfo(info, newAddr, types.A);
+            addArray(child, newAddr, info, firsts);
+            return;
+        }
+        addToInfo(info, newAddr, types.O);
+        addObject(child, newAddr, info, firsts);
+    });
+};
+
+const addObject = (
+    value: any,
+    address: string,
+    info: Map<Map<boolean>>,
+    firsts: Map<string[]>,
+): void => {
+    let keys = firsts[address];
+    if (keys === undefined) {
+        keys = Object.keys(value);
+        firsts[address] = keys;
+        keys.forEach((key) => {
+            createEntry(info, address + addressSeparator + key);
+        });
+    } else {
+        keys.forEach((key) => {
+            if (value[key] === undefined) {
+                addToInfo(info, address + addressSeparator + key, types.E);
+            }
         });
     }
+
+    Object.keys(value).forEach((key) => {
+        const val = value[key];
+        const type = isBasic(val);
+        const addr = address + addressSeparator + key;
+        if (type !== "") {
+            addToInfoExpect(info, addr, type);
+            return;
+        }
+        if (Array.isArray(val)) {
+            addToInfoExpect(info, addr, types.A);
+            addArray(val, addr, info, firsts);
+            return;
+        }
+        addToInfoExpect(info, addr, types.O);
+        addObject(val, addr, info, firsts);
+    });
 };
 
 const ence = (json: string) => {
     let value = JSON.parse(json);
 
-    const info = new TypeInfo();
-    addToInfo(value, "", info);
+    let type = isBasic(value);
+    if (type !== "") {
+        return typePrefix + type;
+    }
 
-    return info.toString();
+    const info: Map<Map<boolean>> = {};
+
+    if (Array.isArray(value)) {
+        addToInfo(info, "", types.A);
+        addArray(value, "", info, {});
+    } else {
+        addToInfo(info, "", types.O);
+        addObject(value, "", info, {});
+    }
+
+    return joinInfo(info);
 };
 
 export default ence;
