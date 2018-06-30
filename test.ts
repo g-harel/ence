fbf87473445b5d6eb4044f5bc18cbdf5856dff86
Gabriel <gabrielj.harel@gmail.com>
2018-06-29 21:06:16 -0400

    move around and add examples

--- test.ts
+++ test.ts
@@ -1,9 +1,13 @@
 import chalk from "chalk";
 import fs from "fs";
-import globby from "globby";
 import path from "path";
 
 import ence from ".";
+
+const examplesPath = "examples";
+const examplesInput = "input.json";
+const examplesOutput = "output.out";
+const benchCount = 32;
 
 test("should be a function", () => {
     expect(ence).toBeInstanceOf(Function);
@@ -24,30 +28,44 @@ test("should return a string", () => {
 });
 
 test("should match the examples", async () => {
-    const paths = await globby("./examples/*.json");
+    const dir = path.join(__dirname, examplesPath);
 
     // load file data from example directory
-    const cases = paths.map((sourcePath) => {
-        const outPath = sourcePath.replace(/\.json$/, ".out");
+    const cases = fs
+        .readdirSync(dir)
+        .filter((name) => {
+            const childPath = path.join(dir, name);
+            return fs.lstatSync(childPath).isDirectory();
+        })
+        .map((name) => {
+            const dirPath = path.join(dir, name);
 
-        if (!fs.existsSync(outPath)) {
-            throw "missing `.out` file for " + path.basename(sourcePath);
-        }
+            const inputPath = path.join(dirPath, examplesInput);
+            const outputPath = path.join(dirPath, examplesOutput);
 
-        return {
-            name: path.basename(sourcePath).replace(/\..+$/g, ""),
-            input: fs.readFileSync(sourcePath, "utf8"),
-            expected: fs.readFileSync(outPath, "utf8"),
-            results: [] as number[],
-        };
-    });
+            if (!fs.existsSync(inputPath)) {
+                throw `missing ${examplesInput} file for ${name}`;
+            }
+            if (!fs.existsSync(outputPath)) {
+                throw `missing ${examplesOutput} file for ${name}`;
+            }
+
+            return {
+                name,
+                input: fs.readFileSync(inputPath, "utf8"),
+                expected: fs.readFileSync(outputPath, "utf8"),
+                results: [] as number[],
+            };
+        })
+        .sort(({input: a}, {input: b}) => {
+            return a.length > b.length ? 1 : -1;
+        });
 
     // run all cases and record perfomance data
     cases.forEach(({input, expected, results}) => {
         expect(ence(input)).toBe(expected.trimRight());
 
-        const count = 32;
-        for (let i = 0; i < count; i++) {
+        for (let i = 0; i < benchCount; i++) {
             const start = process.hrtime();
             ence(input);
             const [s, ns] = process.hrtime(start);
